be1d3383d42c24d28bffaa58dd47fa1aca5066fb
ghosh.debasish <ghosh.debasish@83660578-9083-11dd-b12a-7d853de2e29b>
2008-10-04 15:50:49 +0000

    fixed pattern match with @unmatched

--- src/scala/main/com/dg/collection/immutable/RAList.scala
+++ src/scala/main/com/dg/collection/immutable/RAList.scala
@@ -7,7 +7,7 @@ package com.dg.collection.immutable
  */
 object RAList {
   def empty[T]: RAList[T] = RAList.Nil
-  
+
   private [immutable] case object Nil extends RAList[Nothing] {
     override def equals(that : Any) = this eq that.asInstanceOf[AnyRef] 
   }
@@ -38,11 +38,10 @@ sealed abstract class RAList[+T] {
           lookup(rest, i - size)
         }
     }
-    this match {
+    (this: @unchecked) match {
       case Nil => throw new NoSuchElementException
       case Root(trees) =>
         lookup(trees, i)
-      case _ => throw new IllegalStateException
     }
   }
   
@@ -58,11 +57,10 @@ sealed abstract class RAList[+T] {
    *  @return the first element of this list.
    *  @throws Predef.NoSuchElementException if the list is empty.
    */
-  def head: T = this match {
+  def head: T = (this: @unchecked) match {
     case Nil => throw new NoSuchElementException("head on empty list")
     case Root((size, Leaf(x)) :: rest) => x
     case Root((size, Node(x, t1, t2)) :: rest) => x
-    case _ => throw new IllegalStateException
   }
   
   /** 
@@ -71,7 +69,7 @@ sealed abstract class RAList[+T] {
    *  @return this list without its first element.
    *  @throws Predef.NoSuchElementException if the list is empty.
    */
-  def tail: RAList[T] = this match {
+  def tail: RAList[T] = (this: @unchecked) match {
     case Nil => throw new NoSuchElementException("tail on empty list")
     case Root((size, Leaf(x)) :: rest) => rest match {
         case scala.Nil => Nil
@@ -80,7 +78,6 @@ sealed abstract class RAList[+T] {
     case Root((size, Node(x, t1, t2)) :: rest) =>
       val s = size / 2
       Root((s, t1) :: (s, t2) :: rest)
-    case _ => throw new IllegalStateException
   }
   
   /** 
@@ -90,7 +87,7 @@ sealed abstract class RAList[+T] {
    *  @return  the list with <code>x</code> added at the beginning.
    *  @ex <code>1 :: RAList(2, 3) = RAList(2, 3).::(1) = RAList(1, 2, 3)</code>
    */
-  def ::[U >: T](x: U): RAList[U] = this match {
+  def ::[U >: T](x: U): RAList[U] = (this: @unchecked) match {
     case Root(xs @ ((size1, t1) :: (size2, t2) :: rest)) =>
       if (size1 == size2) {
         Root((1 + size1 + size2, Node(x, t1, t2)) :: rest)
@@ -101,7 +98,6 @@ sealed abstract class RAList[+T] {
       Root((1, Leaf(x)) :: xs)
     case Nil =>
       Root(List((1, Leaf(x))))
-    case _ => throw new IllegalStateException
   }
   
   /** 
@@ -121,10 +117,9 @@ sealed abstract class RAList[+T] {
           (size, t) :: update_a(rest, (i - size), y)
         }
     }
-    this match {
+    (this: @unchecked) match {
       case Root(trees) =>
         Root(update_a(trees, i, value))
-      case _ => throw new IllegalStateException
     }
   }
   
